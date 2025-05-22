ee38e50fd9e52d61059b69b529730ae548b12093
Nir Mendel <mcmendel@gmail.com>
2025-05-22 15:25:51 +0300

    add fetch pokemon and bump to 0.2.1

---
+++ pokemendel_core/data/__init__.py
@@ -0,0 +1,12 @@
+from .gen1 import NAME_TO_POKEMON as GEN1_NAME_TO_POKEMON, Pokemon
+from .gen2 import NAME_TO_POKEMON as GEN2_NAME_TO_POKEMON
+
+__all__ = ["GEN1_NAME_TO_POKEMON", "GEN2_NAME_TO_POKEMON"]
+
+
+def fetch_pokemon(name: str, gen: int) -> Pokemon:
+    if gen == 1:
+        return GEN1_NAME_TO_POKEMON[name]
+    elif gen == 2:
+        return GEN2_NAME_TO_POKEMON[name]
+    raise ValueError(f"No data for generation {gen}")
